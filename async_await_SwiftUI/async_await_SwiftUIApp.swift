dd977cc5adf9db4234a78f187ec5976a01173484
woodycatliu <bestlifeforzhi@gmail.com>
2022-12-09 10:12:34 +0800

    Initial Commit

---
+++ async_await_SwiftUI/async_await_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  async_await_SwiftUIApp.swift
+//  async_await_SwiftUI
+//
+//  Created by Woody Liu on 2022/12/9.
+//
+
+import SwiftUI
+
+@main
+struct async_await_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
